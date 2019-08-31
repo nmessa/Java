fb6356ed3662ebdb112a4d39e7faee051ac4f362
nmessa <nmessa@sau16.org>
2019-08-31 12:57:26 -0400

    Add files via upload

---
+++ Lab Exercise 9.3.2019/Quadratic2/src/Quadratic2.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)Quadratic2.java
+ *
+ * Quadratic2 application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/3
+ */
+
+public class Quadratic2 {
+
+    public static void main(String[] args) {
+		//Declare variables
+    	double x = 0.0;
+    	double fx;
+    	
+    	//Add code here
+    	
+    }
+}
+
+//Output
+//At X = 0.0 the value is 4.0
+//At X = 2.0 the value is 0.0
+//At X = 4.0 the value is 20.0
