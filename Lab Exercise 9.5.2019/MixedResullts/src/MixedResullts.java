fc2bca3445b97ce2ebeb06f81941a93882472688
nmessa <nmessa@sau16.org>
2019-08-31 13:04:55 -0400

    Add files via upload

---
+++ Lab Exercise 9.5.2019/MixedResullts/src/MixedResullts.java
@@ -0,0 +1,33 @@
+/**
+ * @(#)MixedResullts.java
+ *
+ * MixedResullts application
+ *
+ * @Author:  
+ * @version 1.00 2019/9/5
+ */
+ 
+public class MixedResullts {
+    
+    public static void main(String[] args) {
+    	
+    	//Initialize variables
+		double d1 = 37.9; 
+		double d2 = 1004.128;
+		int i1 = 12;
+		int i2 = 18;
+		
+		//Output the results of operations
+		
+		//Add code here
+		
+		
+    }
+}
+
+//Output
+//Problem 1: 1.0
+//Problem 2: 39.13333333333333
+//Problem 3: -1345.3999999999999
+//Problem 4: -1337
+//Problem 5: -1313
