26db6f68e350b59715ef59077bbd12a587b55ca1
nmessa <nmessa@sau16.org>
2019-08-31 13:03:00 -0400

    Add files via upload

---
+++ Lab Exercise 9.5.2019/Calculate/src/Calculate.java
@@ -0,0 +1,53 @@
+/**
+ * @(#)Calculate.java
+ *
+ * Calculate application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/5
+ */
+ import javax.swing.JOptionPane;
+
+ public class Calculate {
+
+    public static void main(String[] args) {
+
+	  //Declare Variables
+      String firstNumber;  	// first string entered by user
+      String secondNumber; 	// second string entered by user
+      String thirdNumber;  	// third string entered by user
+
+      int number1; 	// first number
+      int number2; 	// second number
+      int number3; 	// third number
+
+      int sum;      	// sum of the numbers
+      
+      String result;  //a string to hold the result
+
+      /* write a series of statements to read in three numbers, convert 
+       them to integers, and assign them to number1, number2, 
+       and number3 */
+      
+      //Add code here
+      
+
+      // perform calculations to calculate the sum of 3 numbers
+      //Add code here
+      
+
+      // create result string
+      
+      result = "Answer = " + sum;
+
+      /* Write a statement that concatenates all the results into a 
+       single string that shows the result such as 12 + 7 + 8 = 27 */
+
+      // display results in a message dialog
+      //Add code here
+      
+      
+	  //end program
+      System.exit( 0 );
+    }
+}
