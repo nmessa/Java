aece1824060e9c1f0220ea7aacd2c4c860f766f5
nmessa <nmessa@sau16.org>
2019-08-31 13:00:14 -0400

    Add files via upload

---
+++ Lab Exercise 9.4.2019/StdGaussian/src/StdGaussian.java
@@ -0,0 +1,39 @@
+/**
+ * @(#)StdGaussian.java
+ *
+ * StdGaussian application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/4
+ */
+
+public class StdGaussian {
+
+    public static void main(String[] args) {
+		//Declare variables
+    	double u, v, z, term1, term2;
+    	
+    	
+    	//Generate 2 random values (0.0 to 1.0 and store in u and v)
+    	//Add code here
+    	
+    	
+    	//calculate term1 and term2
+    	//Add code here
+    	
+    	
+    	//store the product of term1 and term2 in z
+    	//Add code here
+    	
+    	
+    	//Print u, v, and z
+    	//Add code here
+    	
+    	
+    }
+}
+
+//Sample output (will be random)
+//u = 0.6824344348223137
+//v = 0.49295478748590926
+//Z = 0.038683814074072904
