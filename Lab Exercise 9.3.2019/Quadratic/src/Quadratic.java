004b7d22556a4097293291f17d4c7f4025485721
nmessa <nmessa@sau16.org>
2019-08-31 12:56:40 -0400

    Add files via upload

---
+++ Lab Exercise 9.3.2019/Quadratic/src/Quadratic.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)Quadratic.java
+ *
+ * Quadratic application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/3
+ */
+
+public class Quadratic {
+
+    public static void main(String[] args) {
+		//Declare variables
+		double x = 2.0/3;
+    	double fx;
+    	
+    	//Add code here
+    	
+    }
+}
+
+//Output
+//At X = 0.6666666666666666 the value is 0.0
