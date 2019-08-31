f21512485309ff6428336c1f47320f56431d02ef
nmessa <nmessa@sau16.org>
2019-08-31 13:01:02 -0400

    Add files via upload

---
+++ Lab Exercise 9.4.2019/TrigIdenty/src/TrigIdenty.java
@@ -0,0 +1,35 @@
+/**
+ * @(#)TrigIdenty.java
+ *
+ * TrigIdenty application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/4
+ */
+
+public class TrigIdenty {
+
+    public static void main(String[] args) {
+		//declare variables
+    	double theta, sum, rad;
+    	
+    	//Assign test angle (10 degrees)
+    	theta = 10;
+    	
+    	//Convert angle to radians
+    	//Add code here
+    	
+    	
+    	//Calculate sum of sin^2 + cos^2
+    	//Add code here
+    	
+    	
+    	//Output the sum
+    	//Add code here
+    	
+    	
+    }
+}
+
+//Output
+//0.9999999999999999
