01340d6d0631d2f7b0307104486af6cbefac7ea7
nmessa <nmessa@sau16.org>
2019-08-31 13:09:55 -0400

    Add files via upload

---
+++ Lab Exercise 9.6.2019/SpeedOfLight/src/SpeedOfLight.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)SpeedOfLight.java
+ *
+ * SpeedOfLight application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/6
+ */
+
+public class SpeedOfLight {
+
+    public static void main(String[] args) {
+    	//Define C as the speed of light in meters/second
+    	//defined constant - note capitalized case style
+    	final double C = 3e8;
+    	
+    	//Declare and intialize variable seconds and intialize to
+    	//the number of seconds in a year (ignore leap years)
+        double seconds = 365 * 24 * 60 * 60;
+        double meters;
+        
+        //Calculate distance in meters light travels in a year
+        //Add code here
+        
+        
+        //Output result
+        //Add code here
+        
+        
+    }
+}
+
+//Output
+//9.4608E15 meters
