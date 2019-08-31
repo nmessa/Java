b83519175be3bbbce8f2a06b221d060008ae23b0
nmessa <nmessa@sau16.org>
2019-08-31 13:08:06 -0400

    Add files via upload

---
+++ Lab Exercise 9.6.2019/CornerDistance/src/CornerDistance.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)CornerDistance.java
+ *
+ * CornerDistance application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/6
+ */
+
+import java.util.*;  //required for Scanner
+
+public class CornerDistance {
+
+    public static void main(String[] args) {
+    	//Create a Scanner object
+    	Scanner reader = new Scanner(System.in);
+    	
+    	//Declare variables
+    	double length, width, distance;
+    	
+    	//Get the width of the rectangle and store in width
+    	//Be sure to use a prompt
+    	//Add code here
+    	
+    	
+    	//Get the length of the rectangle and store in length
+    	//Be sure to use a prompt
+    	//Add code here
+    	
+    	
+    	//Calculate the corner to corner distance of the rectangle
+    	//Add code here
+    	
+    	
+    	//Output the result
+    	System.out.println("Distance = " + distance);
+    }
+}
+
+//Sample Output
+//Enter the width of a rectangle: 10
+//Enter the length of a rectangle: 15
+//Distance = 18.027756377319946
