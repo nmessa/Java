eda69f7eca82ece2eab3508052c31381ba5de0f8
nmessa <nmessa@sau16.org>
2019-08-31 12:58:24 -0400

    Add files via upload

---
+++ Lab Exercise 9.4.2019/CartesianToPolar/src/CartesianToPolar.java
@@ -0,0 +1,40 @@
+/**
+ * @(#)CartesianToPolar.java
+ *
+ * CartesianToPolar application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/4
+ */
+
+public class CartesianToPolar {
+
+    public static void main(String[] args) {
+
+		//Declare variables
+    	double x, y, r, theta, degrees;
+    	
+    	
+    	//set rectangular coordinates
+    	x = 1.0;
+    	y = 2.0;
+    	
+    	//calculate r
+    	//Add code here
+    	
+    	
+    	//calculate theta
+    	//Add code here
+    	
+    	
+    	//convert angle theta from radians to degrees
+    	//Add code here
+    	
+    	
+    	//Output the result
+    	System.out.println("r = " + r + "  theta = " + degrees + " degrees");
+    }
+}
+
+//Output
+//r = 2.23606797749979  theta = 63.43494882292201 degrees
