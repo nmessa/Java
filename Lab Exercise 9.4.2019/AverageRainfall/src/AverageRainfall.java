e2f3eaa9da292af06059902fe31beaa6aebc5641
nmessa <nmessa@sau16.org>
2019-08-31 12:55:25 -0400

    Add files via upload

---
+++ Lab Exercise 9.4.2019/AverageRainfall/src/AverageRainfall.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)AverageRainfall.java
+ *
+ * AverageRainfall application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/4
+ */
+
+public class AverageRainfall {
+
+    public static void main(String[] args) {
+
+		//Declare and initialize variables
+    	int april = 12, may = 14, june = 8;
+    	double average;
+    	
+
+		//Calculate the average
+    	//Add code here
+    	
+
+		//Output report
+    	//Add code here
+    	
+    	
+    }
+}
+
+//Output
+//Rainfall for April: 12
+//Rainfall for May:   14
+//Rainfall for June:  8
+//Average Rainfall:   11.333333333333334
