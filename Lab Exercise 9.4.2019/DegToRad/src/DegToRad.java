57265e23ea44b59ed88786c810a83940fc1d7343
nmessa <nmessa@sau16.org>
2019-08-31 12:59:25 -0400

    Add files via upload

---
+++ Lab Exercise 9.4.2019/DegToRad/src/DegToRad.java
@@ -0,0 +1,40 @@
+/**
+ * @(#)DegToRad.java
+ *
+ * DegToRad application
+ *
+ * @Author: 
+ * @version 1.00 2019/9/4
+ */
+
+public class DegToRad {
+
+    public static void main(String[] args) {
+		//Declare and initialize variables
+    	double sinx, cosx, sum, rad;
+    	
+    	double degrees = 30.0; //put in test value
+    	
+    	//Convert degrees to radians
+		//Add code here
+    	
+		
+		//Calculate the sine of the angle
+    	//Add code here
+    	
+    	
+    	//Calculate the cosine of the angle
+    	//Add code here
+    	
+    	
+    	//Calculate the sum of sin^2 + cos^2
+    	//Add code here
+    	
+    	
+    	//Output result
+    	System.out.println("sine: " + sinx + " cosine: " + cosx + " sum: " + sum );
+    }
+}
+
+//Output
+//sine: 0.49999999999999994 cosine: 0.8660254037844387 sum: 1.0
