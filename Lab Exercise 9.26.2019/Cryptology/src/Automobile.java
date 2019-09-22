dbe63830042ff9fd15c34b132f46734a8340b9c6
nmessa <nmessa@sau16.org>
2019-09-22 12:34:34 -0400

    Add files via upload

---
+++ Lab Exercise 9.26.2019/Cryptology/src/Automobile.java
@@ -0,0 +1,37 @@
+//Automobile Class
+//Author: 
+//Date: 9/25/2019
+
+public class Automobile {
+	//Instance fields
+	private int mpg;
+	private int gallons;
+	
+	//Constructor that initializes the mpg
+	public Automobile(int m)
+	{
+		//Add code here
+    	
+	}
+	
+	//The method adds g to gallons
+	public void fillUp(int g)
+	{
+		//Add code here
+    	
+	}
+	
+	//This method updates gallons in tank based on miles traveled
+	public void takeTrip(int miles)
+	{
+		//Add code here
+    	
+	}
+	
+	//This method returns the contents of private instance field
+	public int reportFuel()
+	{
+		//Add code here
+    	
+	}
+}
