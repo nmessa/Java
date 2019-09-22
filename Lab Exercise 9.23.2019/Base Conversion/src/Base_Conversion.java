88443f0e95499c1b85cb8fad6d60f4582265158d
nmessa <nmessa@sau16.org>
2019-09-22 13:16:56 -0400

    Add files via upload

--- Lab Exercise 9.23.2019/Base Conversion/src/Base_Conversion.java
+++ Lab Exercise 9.23.2019/Base Conversion/src/Base_Conversion.java
@@ -3,42 +3,45 @@
  *
  * Base_Conversion application
  *
- * @Author:  
+ * @nmessa 
  * @version 1.00 2019/9/23
  */
  
 public class Base_Conversion {
     
     public static void main(String[] args) {
-    	//declare variables
+    	
     	int binary, octal, hex, decimal;
     	String sbin, soct, shex, wierd;
     	decimal = 42;
     	
     	//convert decimal value to different base
-    	//Add code here
-    	
+    	sbin = Integer.toBinaryString(decimal);
+    	soct = Integer.toOctalString(decimal);
+    	shex = Integer.toHexString(decimal);
     	
     	//print results as strings
-    	//Add code here
-    	
-    	
+    	System.out.println(decimal + " = " + sbin + " base 2");
+    	System.out.println(decimal + " = " + soct + " base 8");
+    	System.out.println(decimal + " = " + shex + " base 16");
     	
     	//Turn strings back to numbers that are base 10
-    	//Add code here
-    	
+    	binary = Integer.parseInt(sbin, 2);
+    	octal = Integer.parseInt(soct, 8);
+    	hex = Integer.parseInt(shex, 16);
     	
     	//print results as numbers
-    	//Add code here
-    	
+    	System.out.println(binary);
+    	System.out.println(octal);
+    	System.out.println(hex);
     	
     	//Converting to a weird base
-    	//Add code here
-    	
+    	wierd = Integer.toString(5879187, 35);
+    	System.out.println("5879187 = " + wierd + " base 35");
     	
     	//Convert base 35 number back to base 10
-    	//Add code here
-    	
+    	decimal = Integer.parseInt(wierd, 35);
+    	System.out.println(wierd + " base 35 = " + decimal);
     }
 }
 
